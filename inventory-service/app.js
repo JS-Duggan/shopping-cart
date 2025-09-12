2337cd61c2f2b4ad653b1170f4895570320ec4d1
JS-Duggan <jacksdggn@gmail.com>
2025-09-12 16:02:37 +0930

    feat: implement db

--- inventory-service/app.js
+++ inventory-service/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const pg = require('pg');
+
 const app = express();
 const port = process.env.PORT;
+const client = new pg.Client({
+    host: 'psql',
+    user: 'root',
+    password: 'password',
+    database: 'inventory'
+});
+
+client.connect()
+    .then(() => console.log('connected to DB'))
+    .catch((err) => console.error(`Error connecting to DB: ${err}`));
+
 
 app.get('/test', (req, res) => res.send('testing...'));
-
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 });
